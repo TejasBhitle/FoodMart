9afbdff7ccc76e1af43908a6dafefede4431b9c5
TejasBhitle <bhitletejas@gmail.com>
2017-09-25 22:55:03 +0530

    Routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BootstrapNavbarComponent } from './bootstrap-navbar/bootstrap-navbar.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component'
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { CheckOutComponent } from './check-out/check-out.component';
@@ -28,13 +29,24 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
     OrderSuccessComponent,
     MyOrdersComponent,
     AdminProductsComponent,
-    AdminOrdersComponent
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      { path:'', component:HomeComponent },
+      { path:'products', component:ProductsComponent },
+      { path:'shopping-cart', component:ShoppingCartComponent },
+      { path:'check-out', component:CheckOutComponent },
+      { path:'order-success', component:OrderSuccessComponent },
+      { path:'login', component:LoginComponent },
+      { path:'admin/products', component:AdminProductsComponent },
+      { path:'admin/orders', component:AdminOrdersComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
