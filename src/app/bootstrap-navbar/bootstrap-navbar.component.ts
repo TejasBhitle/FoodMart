c376754c58588162387a7916491ecfd2ff77714c
TejasBhitle <bhitletejas@gmail.com>
2017-09-30 18:10:12 +0530

    Firebase Authentication added

--- src/app/bootstrap-navbar/bootstrap-navbar.component.ts
+++ src/app/bootstrap-navbar/bootstrap-navbar.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'bootstrap-navbar',
   templateUrl: './bootstrap-navbar.component.html',
   styleUrls: ['./bootstrap-navbar.component.css']
 })
-export class BootstrapNavbarComponent implements OnInit {
+export class BootstrapNavbarComponent {
 
-  constructor() { }
+  user : firebase.User;
 
-  ngOnInit() {
+  constructor(private afAuth:AngularFireAuth) {
+    afAuth.authState.subscribe(user => this.user = user );
+  }
+
+  logout(){
+    this.afAuth.auth.signOut();
   }
 
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  constructor(private afAuth:AngularFireAuth) {
 
-  ngOnInit() {
   }
 
+  login(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    this.afAuth.auth.signInWithRedirect(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(token+" "+user);
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      console.log("Error");
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      console.log("error: "+" "+error.name+" "+errorMessage);
+      // ...
+    });
+  }
 }
