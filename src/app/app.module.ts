b8842e44e94d56bc38196e94fb12dafdee5c2327
TejasBhitle <bhitletejas@gmail.com>
2018-01-27 19:35:16 +0530

    seperate product quantity component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { CustomFormsModule } from 'ng2-validation';
 import { DataTableModule } from 'angular-4-data-table';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
 import { ProductCardComponent } from './product-card/product-card.component';
+import { ProductQuantityComponent } from './product-quantity/product-quantity.component';
 
 @NgModule({
   declarations: [
@@ -49,7 +50,8 @@ import { ProductCardComponent } from './product-card/product-card.component';
     LoginComponent,
     ProductFormComponent,
     ProductFilterComponent,
-    ProductCardComponent
+    ProductCardComponent,
+    ProductQuantityComponent
   ],
   imports: [
     BrowserModule,
--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,9 +1,20 @@
 import { ShoppingCartItem } from './shopping-cart-item';
+import { Product } from './product';
 
 export class ShoppingCart{
 
-  constructor(public items: ShoppingCartItem[]){
+  items: ShoppingCartItem[] = [];
 
+  constructor(public itemsMap: { [productId: string]: ShoppingCartItem}) {
+    for(let productId in itemsMap){
+      let item = itemsMap[productId];
+      this.items.push(new ShoppingCartItem(item.product,item.quantity));
+    }
+  }
+
+  getQuantity(product: Product){
+    let item = this.itemsMap[product.$key];
+    return item ? item.quantity : 0;
   }
 
   get totalItemsCount(): number {
@@ -11,5 +22,13 @@ export class ShoppingCart{
     for(let productId in this.items)
       count += this.items[productId].quantity;
     return count;
+  }
+
+  get totalPrice(): number{
+    let total = 0;
+    for(let productId in this.items){
+      total += this.items[productId].totalPrice;
+    }
+    return total;
   }
 }
